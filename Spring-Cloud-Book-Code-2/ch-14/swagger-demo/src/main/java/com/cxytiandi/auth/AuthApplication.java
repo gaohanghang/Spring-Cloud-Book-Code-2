5e8f163eeee4b3ee4d27d828c9770ec9b7509993
gaohanghang <1341947277@qq.com>
2019-09-22 17:21:24 +0800

    <更新>[swagger]: 添加log日志输出

--- Spring-Cloud-Book-Code-2/ch-14/swagger-demo/src/main/java/com/cxytiandi/auth/AuthApplication.java
+++ Spring-Cloud-Book-Code-2/ch-14/swagger-demo/src/main/java/com/cxytiandi/auth/AuthApplication.java
@@ -1,5 +1,6 @@
 package com.cxytiandi.auth;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,8 +10,10 @@ import com.spring4all.swagger.EnableSwagger2Doc;
 @EnableDiscoveryClient
 @EnableSwagger2Doc
 @SpringBootApplication
+@Slf4j
 public class AuthApplication {
 	public static void main(String[] args) {
 	    SpringApplication.run(AuthApplication.class, args);
+		System.out.println("http://localhost:8081/swagger-ui.html");
 	}
 }
---
+++ Spring-Cloud-Book-Code-2/ch-14/swagger-zuul/README.md
@@ -0,0 +1,4 @@
+# Zuul 中聚合多个服务 Swagger
+
+![](https://raw.githubusercontent.com/gaohanghang/images/master/img/20190922171927.png)
+
